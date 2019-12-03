0f8b69ade2eeb7e25d4e81800b90c6ca8ff4e96d
Ethiraric <floriansabourin@live.fr>
2019-12-03 18:29:52 +0100

    Move Talk json (de)serialization to heaader file.

--- include/cppti/Talk.hh
+++ include/cppti/Talk.hh
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 
+#include <nlohmann/json.hpp>
+
 namespace cppti
 {
 struct Talk
@@ -16,6 +18,33 @@ struct Talk
   std::vector<std::string> tags;
 };
 using Talks = std::vector<Talk>;
+}
+
+namespace nlohmann
+{
+void from_json(json const& j, cppti::Talk& x);
+void to_json(json& j, cppti::Talk const& x);
+
+inline void from_json(json const& j, cppti::Talk& x)
+{
+  x.speakers = j.at("speakers").get<std::vector<std::string>>();
+  x.title = j.at("title").get<std::string>();
+  x.conference = j.at("conference").get<std::string>();
+  x.year = j.at("year").get<int64_t>();
+  x.link = j.at("link").get<std::string>();
+  x.tags = j.at("tags").get<std::vector<std::string>>();
+}
+
+inline void to_json(json& j, cppti::Talk const& x)
+{
+  j = json::object();
+  j["speakers"] = x.speakers;
+  j["title"] = x.title;
+  j["conference"] = x.conference;
+  j["year"] = x.year;
+  j["link"] = x.link;
+  j["tags"] = x.tags;
+}
 }
 
 #endif /* !CPPTI_TALK */
--- src/TalksLoader.cpp
+++ src/TalksLoader.cpp
@@ -2,35 +2,6 @@
 
 #include <fstream>
 
-#include <nlohmann/json.hpp>
-
-namespace nlohmann
-{
-void from_json(json const& j, cppti::Talk& x);
-void to_json(json& j, cppti::Talk const& x);
-
-inline void from_json(json const& j, cppti::Talk& x)
-{
-  x.speakers = j.at("speakers").get<std::vector<std::string>>();
-  x.title = j.at("title").get<std::string>();
-  x.conference = j.at("conference").get<std::string>();
-  x.year = j.at("year").get<int64_t>();
-  x.link = j.at("link").get<std::string>();
-  x.tags = j.at("tags").get<std::vector<std::string>>();
-}
-
-inline void to_json(json& j, cppti::Talk const& x)
-{
-  j = json::object();
-  j["speakers"] = x.speakers;
-  j["title"] = x.title;
-  j["conference"] = x.conference;
-  j["year"] = x.year;
-  j["link"] = x.link;
-  j["tags"] = x.tags;
-}
-}
-
 namespace cppti
 {
 Expected<Talks> TalksLoader::loadFrom(std::string_view file)
